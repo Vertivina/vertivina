fd1d036899e8826730777bd799bead7e5bef4d52
Alvarden <31715989+Alvarden15@users.noreply.github.com>
2018-10-24 12:33:02 -0500

    Actualización de las migraciones

---
+++ Migrations/20181024172722_Mejora3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace pangu.Migrations
+{
+    public partial class Mejora3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hora",
+                table: "Cita",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hora",
+                table: "Cita");
+        }
+    }
+}
