4fb2f653ba65f331b17de8c2ccfb8cc61d5475c7
olacunza <oresteslacunza@hotmail.com>
2018-10-27 14:10:53 -0500

    Pestaña Accesorios

--- Controllers/Productos2Controller.cs
+++ Controllers/Productos2Controller.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Models.MvcContext;
 
 namespace vertivina.Controllers
 {
     public class Productos2Controller : Controller
     {
+        private readonly MvcContext _context;
+        public Productos2Controller(MvcContext context){
+            this._context = context;
+        }
         public IActionResult Index()
         {
+            //var productos = from m in _context.Productos2 select m;
+            //return View(productos);
             return View();
         }
+
 
     }
 }
