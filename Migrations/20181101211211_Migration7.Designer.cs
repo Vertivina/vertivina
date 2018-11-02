c9224ffa04d470871af21ce67546c6de89507b0e
Alvarden <31715989+Alvarden15@users.noreply.github.com>
2018-11-01 19:10:57 -0500

    Cambios de interfaces

--- Migrations/20181101211211_Migration7.Designer.cs
+++ Migrations/20181101211211_Migration7.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace vertivina.Migrations
 {
     [DbContext(typeof(MvcContext))]
-    partial class MvcContextModelSnapshot : ModelSnapshot
+    [Migration("20181101211211_Migration7")]
+    partial class Migration7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
