2b4f4985869d1778f3876182677a4abaa995d70b
Alvarden <31715989+Alvarden15@users.noreply.github.com>
2018-11-08 12:36:54 -0500

    Nuevo atributo para mascota: imagen
    Nuevo atributo que perimitirá usar la imagen de la mascota solo usando un link
    Como ayuda: para usarlo deben utilizar:
    <img src="@Url.Content(model.[atributo donde esta el link])" >
    Cuando se esté listando, cambien model por item, puesto que esta dentro de un foreach.
    Suerte con los cambios que esten haciendo

---
+++ Migrations/20181108173315_Migracion2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pangu.Migrations
+{
+    public partial class Migracion2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imagen",
+                table: "Mascota",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imagen",
+                table: "Mascota");
+        }
+    }
+}
